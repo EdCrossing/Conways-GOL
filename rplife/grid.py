d95c35bf7f43d8c86ec5abf196f6eb1e7798138b
EdCrossing <crossing200@gmail.com>
2024-06-17 15:54:52 +0100

    update project files with latest changes

--- rplife/grid.py
+++ rplife/grid.py
@@ -1,5 +1,8 @@
 import collections
 
+ALIVE = "X"
+DEAD = "‧"
+
 class LifeGrid:
     def __init__(self, pattern):
         self.pattern = pattern
@@ -28,7 +31,13 @@ class LifeGrid:
         self.pattern.alive_cells = stay_alive | come_alive
 
     def as_string(self, bbox):
-        pass
+        start_col, start_row, end_col, end_row = bbox
+        display = [self.pattern.name.center(2*(end_col-start_col))]
+        for row in range(start_row, end_row):
+            display_row = [ALIVE if (row,col) in self.pattern.alive_cells else DEAD for col in range(start_col, end_col)] 
+            display.append(" ".join(display_row))
+        return "\n ".join(display)
+
     def __str__(self):
         return (
             f"{self.pattern.name}:\n"
